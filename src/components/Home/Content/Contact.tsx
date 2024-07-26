228e9a80818e020c2568c04ce6aed4e59be7bb58
Luis Herrera <luis1818.lh@gmail.com>
2024-07-26 17:21:17 -0500

    create Contact components and create style for buttons

--- src/components/Home/Content/Contact.tsx
+++ src/components/Home/Content/Contact.tsx
@@ -1,6 +1,23 @@
+import { SubContentLanguage } from "../../../type"
+import style from "./Contact.module.css"
 
-export const Contact = () => {
+type PropsContact = {
+    title: string,
+    subContent: SubContentLanguage
+}
+
+export const Contact = ({title, subContent }: PropsContact) => {
   return (
-    <div>Contact</div>
+    <section className={style.contactContainer}>
+        <h2 className='title2'>{title}</h2>
+        <div className={style.containerContentContact}>
+            <div className={style.textContact}>
+                <p className="paragraph">{subContent.p1}</p>
+                <p className="paragraph">{subContent.p2}</p>
+            </div>
+            <button className={`button-text button-style ${style.buttonSendEmail}`}>{subContent.sendEmailButton}</button>
+        </div>
+        
+    </section>
   )
 }
--- src/components/Home/Content/Content.tsx
+++ src/components/Home/Content/Content.tsx
@@ -1,18 +1,21 @@
 import { useContext } from 'react'
 import style from './Content.module.css'
 import { LanguageContext } from '../../../context/LanguageContext'
+import { Contact } from './Contact'
 export const Content = () => {
   const context = useContext(LanguageContext)
-  if(!context){
+  if (!context) {
     return null
   }
-  
-  const {titleLanguage} = context
+
+  const { titleLanguage, subContentLanguage } = context
   return (
     <section className={style.container}>
       <article className={`${style.bg} ${style.yellowb}`}><h2 className='title2'>{titleLanguage.about}</h2></article>
       <article className={`${style.bg} ${style.blueb}`}><h2 className='title2'>{titleLanguage.project}</h2></article>
-      <article className={`${style.bg} ${style.redb}`}><h2 className='title2'>{titleLanguage.contact}</h2></article>
+      <article className={`${style.bg} ${style.redb}`}>
+        <Contact title={titleLanguage.contact} subContent={subContentLanguage} />
+      </article>
       <article className={`${style.bg} ${style.blueb}`}><h2 className='title2'>{titleLanguage.curriculum}</h2></article>
       <article className={`${style.bg} ${style.yellowb}`}><h2 className='title2'>{titleLanguage.skills}</h2></article>
     </section>
