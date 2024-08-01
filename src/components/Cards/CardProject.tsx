9e0edf831ee7804ce81b5d35f2bf16e0269856cf
Luis Herrera <luis1818.lh@gmail.com>
2024-08-01 14:11:16 -0500

    modify card Project design

--- src/components/Cards/CardProject.tsx
+++ src/components/Cards/CardProject.tsx
@@ -1,21 +1,30 @@
+import { Project } from '../../type'
 import { Demo, GitHub } from '../Icons'
 import styleCard from './CardProject.module.css'
 
-export const CardProject = () => {
 
-  const urlImage = `/assets/${'calendar'}.webp`
+export const CardProject = (project: Project) => {
+
+  
+  const urlImage = `/assets/${project.image}.webp`
 
   return (
     <article className={styleCard.cardContainer}>
-        <img className={styleCard.imgCard} src={urlImage} alt="imagen proyecto" />
-        <div className={styleCard.containerContent}>
-            <h2 className={`title2`}>Titulo</h2>
-            <p className={`paragraph`}>Crea eventos, revisa y actualiza tu agenda, se más productivo.</p>
-            <div className={styleCard.cardContainerEnlaces}>
-              <a className={`button-text ${styleCard.enlaces}`} href=""><Demo/></a>
-              <a className={`button-text ${styleCard.enlaces}`} href=""><GitHub/></a>
-            </div>
+      <img className={styleCard.imgCard} src={urlImage} alt="imagen proyecto" />
+      <div className={styleCard.containerContent}>
+        <h2 className={`title2`}>{project.name}</h2>
+        <p className={`paragraph`}>{project.description}</p>
+        <div className={styleCard.cardContainerEnlaces}>
+          <a className={`button-text ${styleCard.enlaces}`} href={project.url} target='_blank'
+            rel='noopener noreferrer'>
+            <Demo />
+          </a>
+          <a className={`button-text ${styleCard.enlaces}`} href={project.urlGitHub} target='_blank'
+            rel='noopener noreferrer'>
+            <GitHub />
+          </a>
         </div>
+      </div>
     </article>
   )
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,19 +2,29 @@ import { useContext } from "react"
 import { Layout } from "../Layout"
 import { LanguageContext } from "../context/LanguageContext"
 import { CardProject } from "../components";
+import styleProjects from "./Projects.module.css"
 
 
 export const Projects = () => {
   const context = useContext(LanguageContext);
-  if(!context){
+  if (!context) {
     return null
   }
   const title = context.titleLanguage
-  /* const data = context.data; */
+  const data = context.data;
   return (
     <Layout>
       <h1 className="title1">{title.project}</h1>
-      <CardProject/>
+      <main className={styleProjects.cardMain}>
+        {
+          data?.projects.map(project => (
+
+            <CardProject key={project.name} {...project} />
+
+          ))
+        }
+      </main>
+
     </Layout>
   )
 }
