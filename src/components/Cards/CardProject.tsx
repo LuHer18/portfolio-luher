93cf0c308d3ea21b51e505f9e27362c3644d3a61
Luis Herrera <luis1818.lh@gmail.com>
2024-08-01 17:57:59 -0500

    update design project card

--- src/components/Cards/CardProject.tsx
+++ src/components/Cards/CardProject.tsx
@@ -5,26 +5,30 @@ import styleCard from './CardProject.module.css'
 
 export const CardProject = (project: Project) => {
 
-  
+
   const urlImage = `/assets/${project.image}.webp`
 
   return (
     <article className={styleCard.cardContainer}>
-      <img className={styleCard.imgCard} src={urlImage} alt="imagen proyecto" />
-      <div className={styleCard.containerContent}>
-        <h2 className={`title2`}>{project.name}</h2>
-        <p className={`paragraph`}>{project.description}</p>
-        <div className={styleCard.cardContainerEnlaces}>
-          <a className={`button-text ${styleCard.enlaces}`} href={project.url} target='_blank'
-            rel='noopener noreferrer'>
-            <Demo />
-          </a>
-          <a className={`button-text ${styleCard.enlaces}`} href={project.urlGitHub} target='_blank'
-            rel='noopener noreferrer'>
-            <GitHub />
-          </a>
+
+      <header>
+        <img className={styleCard.imgCard} src={urlImage} alt="imagen proyecto" />
+        <div className={styleCard.containerContent}>
+          <h2 className={`title2`}>{project.name}</h2>
+          <p className={`paragraph`}>{project.description}</p>
         </div>
-      </div>
+      </header>
+
+      <footer className={styleCard.cardContainerEnlaces}>
+        <a className={`button-text ${styleCard.enlaces}`} href={project.url} target='_blank'
+          rel='noopener noreferrer'>
+          <Demo />
+        </a>
+        <a className={`button-text ${styleCard.enlaces}`} href={project.urlGitHub} target='_blank'
+          rel='noopener noreferrer'>
+          <GitHub />
+        </a>
+      </footer>
     </article>
   )
 }
