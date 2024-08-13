4db5655728f3baffc9eda491d2fad208d4cb1eb7
Luis Herrera <luis1818.lh@gmail.com>
2024-08-12 19:23:04 -0500

    se agrega funcionalidad de componentes de skills

--- src/components/Home/Content/Skills.tsx
+++ src/components/Home/Content/Skills.tsx
@@ -9,19 +9,31 @@ export const Skills = ({ title, data }: SkillsProps) => {
     const skills = data?.skills;
 
     return (
-        <section className={style.slider}>
+        <section className={style.container}>
             <h2 className='title2'>{title}</h2>
-            <div className={style.slideTrack}>
-                {
-                    skills?.map(skill => {
-                        const skillImage  = `/assets/icons-skills/${skill.image}.svg`
-                        return (
-                            <div className={style.slide}>
-                                <img src={skillImage} alt={skill.name} />
-                            </div>
-                        )
-                    })
-                }
+            <div className={style.slider}>
+                <div className={style.slideTrack}>
+                    {
+                        skills?.map(skill => {
+                            const skillImage = `/assets/icons-skills/${skill.image}.svg`
+                            return (
+                                <div className={style.slide} key={skill.name}>
+                                    <img className={style.imageSkills} loading='lazy' src={skillImage} alt={skill.name} />
+                                </div>
+                            )
+                        })
+                    }
+                    {
+                        skills?.map(skill => {
+                            const skillImage = `/assets/icons-skills/${skill.image}.svg`
+                            return (
+                                <div className={style.slide} key={skill.name}>
+                                    <img className={style.imageSkills} src={skillImage} alt={skill.name} />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </section>
     )
