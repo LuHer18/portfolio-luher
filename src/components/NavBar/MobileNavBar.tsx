a402889bf80fb6cb2857d3d1bc2abdbe824fd9bc
Luis Herrera <luis1818.lh@gmail.com>
2024-09-18 11:24:05 -0500

    Ahora el boton del navbar mobile permite hacer el toggle correctamente

--- src/components/NavBar/MobileNavBar.tsx
+++ src/components/NavBar/MobileNavBar.tsx
@@ -12,13 +12,19 @@ type PropsMobile = {
 export const MobileNavBar = ({ navLanguage, handleLanguage, language }: PropsMobile) => {
     const [showMenu, setShowMenu] = useState(false)
     const menuRef = useRef<HTMLElement | null>(null);
+    const menuButtonRef = useRef<HTMLButtonElement | null>(null);
 
     const onMenu = () => {
         setShowMenu(!showMenu)
     }
 
     const handleClickOutside = (event: MouseEvent) => {
-        if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        if (
+            menuRef.current &&
+            !menuRef.current.contains(event.target as Node) && 
+            menuButtonRef.current &&
+            !menuButtonRef.current.contains(event.target as Node)
+        ) {
             setShowMenu(false);
         }
     };
@@ -30,13 +36,13 @@ export const MobileNavBar = ({ navLanguage, handleLanguage, language }: PropsMob
         return () => {
             document.removeEventListener('mousedown', handleClickOutside);
         };
-    }, []);
+    }, [showMenu]);
 
 
     return (
         <section className={styleMobileNavBar.container}>
             <div className={styleMobileNavBar.containerButton}>
-                <button className={styleMobileNavBar.buttonMobile} onClick={onMenu} title="Menú">
+                <button className={styleMobileNavBar.buttonMobile} ref={menuButtonRef} onClick={onMenu} title="Menú">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="transparent" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
